31dc0baf1ddd3106de8732bd9f5eb34b7d8f5222
JRuedas <11670560+JRuedas@users.noreply.github.com>
2022-04-25 19:05:20 +0200

    Adds zip to backups

--- actividad1/backup_volumes.sh
+++ actividad1/backup_volumes.sh
@@ -9,7 +9,7 @@ if [ $( docker ps -a | grep drupal | wc -l ) -gt 0 ]; then
     echo "Creating drupal backup ..."
     for folder in modules profiles sites themes
     do
-        docker run --rm --volumes-from drupal -v $(pwd):/backup busybox sh -c "cd ${drupal_dir}/$folder && tar cf /backup/drupal_${folder}_backup.tar ."
+        docker run --rm --volumes-from drupal -v $(pwd):/backup busybox sh -c "cd ${drupal_dir}/$folder && tar cfz /backup/drupal_${folder}_backup.tar.gz ."
     done
     echo "drupal backup created correctly."
 else
@@ -18,7 +18,7 @@ fi
 
 if [ $( docker ps -a | grep db | wc -l ) -gt 0 ]; then
     echo "Creating db backup ..."
-    docker run --rm --volumes-from db -v $(pwd):/backup busybox sh -c "cd ${db_dir} && tar cf /backup/db_backup.tar ."
+    docker run --rm --volumes-from db -v $(pwd):/backup busybox sh -c "cd ${db_dir} && tar cfz /backup/db_backup.tar.gz ."
     echo "db backup created correctly."
 else
     echo "db container is missing."
--- actividad1/restoring_volumes.sh
+++ actividad1/restoring_volumes.sh
@@ -11,10 +11,10 @@ docker-compose up --no-start
 echo "Restoring drupal backup ..."
 for folder in modules profiles sites themes
 do
-    docker run --rm --volumes-from drupal -v $(pwd):/backup busybox sh -c "cd ${drupal_dir}/$folder && tar xf /backup/drupal_${folder}_backup.tar"
+    docker run --rm --volumes-from drupal -v $(pwd):/backup busybox sh -c "cd ${drupal_dir}/$folder && tar xfz /backup/drupal_${folder}_backup.tar.gz"
 done
 echo "drupal backup restored correctly."
 
 echo "Restoring db backup ..."
-docker run --rm --volumes-from db -v $(pwd):/backup busybox sh -c "cd $db_dir && tar xf /backup/db_backup.tar"
+docker run --rm --volumes-from db -v $(pwd):/backup busybox sh -c "cd $db_dir && tar xfz /backup/db_backup.tar.gz"
 echo "db backup restored correctly."
